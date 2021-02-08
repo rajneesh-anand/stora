9534e4197661a7fde82e802991e49f7e30e6fee8
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-02-08 15:24:13 +0530

    orders

--- src/pages/api/fetchOrders.js
+++ src/pages/api/fetchOrders.js
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/client";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const { email } = req.body;
+  const session = await getSession({ req });
+
   console.log(email);
 
   switch (req.method) {
