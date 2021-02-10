208f0eaa1b68db981773358f0afae414a246aad2
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-02-10 23:51:32 +0530

    orders

--- src/pages/api/paytmtest.js
+++ src/pages/api/paytmtest.js
@@ -1,76 +1,61 @@
 const Paytm = require("paytm-pg-node-sdk");
 
 export default async function handler(req, res) {
-  try {
-    const { name, email, mobile, address, pin, amount } = req.body;
-    var env = Paytm.LibraryConstants.STAGING_ENVIRONMENT;
-    var mid = "zWEMTK89662017572077";
-    var key = "4GNIZVo6#kP6FYhF";
-    var website = "WEBSTAGING";
-    //   var client_id = "C15";
+  const { name, email, mobile, address, pin, amount } = req.body;
+  var env = Paytm.LibraryConstants.STAGING_ENVIRONMENT;
+  var mid = "zWEMTK89662017572077";
+  var key = "4GNIZVo6#kP6FYhF";
+  var website = "WEBSTAGING";
+  //   var client_id = "C15";
 
-    var callbackUrl = "https://stora-eight.vercel.app/shop/checkout";
-    Paytm.MerchantProperties.setCallbackUrl(callbackUrl);
-    Paytm.MerchantProperties.initialize(env, mid, key, website);
-    // If you want to add log file to your project, use below code
-    Paytm.Config.logName = "[PAYTM]";
-    Paytm.Config.logLevel = Paytm.LoggingUtil.LogLevel.INFO;
-    Paytm.Config.logfile = "https://stora-eight.vercel.app/logs/file.log";
+  var callbackUrl = "https://stora-eight.vercel.app/shop/checkout";
+  Paytm.MerchantProperties.setCallbackUrl(callbackUrl);
+  Paytm.MerchantProperties.initialize(env, mid, key, website);
 
-    var orderId = "RSGI" + Math.floor(Math.random(6) * 1000000);
-    var channelId = Paytm.EChannelId.WEB;
-    var txnAmount = Paytm.Money.constructWithCurrencyAndValue(
-      Paytm.EnumCurrency.INR,
-      amount
+  var orderId = "RSGI" + Math.floor(Math.random(6) * 1000000);
+  var channelId = Paytm.EChannelId.WEB;
+  var txnAmount = Paytm.Money.constructWithCurrencyAndValue(
+    Paytm.EnumCurrency.INR,
+    amount
+  );
+  var userInfo = new Paytm.UserInfo(email);
+  userInfo.setAddress(address);
+  userInfo.setEmail(email);
+  userInfo.setFirstName(name);
+  userInfo.setMobile(mobile);
+  userInfo.setPincode(pin);
+
+  var goodsInfo = new Paytm.GoodsInfo();
+  goodsInfo.setMerchantGoodsId("goods_id1");
+  goodsInfo.setMerchantShippingId("shipping_id1");
+  goodsInfo.setSnapshotUrl("snapshot_url");
+  goodsInfo.setDescription("desc");
+  goodsInfo.setCategory("category");
+  goodsInfo.setQuantity("qty");
+  goodsInfo.setUnit("unit");
+  goodsInfo.setPrice(
+    Paytm.Money.constructWithCurrencyAndValue(Paytm.EnumCurrency.INR, "1.00")
+  );
+
+  var paymentDetailBuilder = new Paytm.PaymentDetailBuilder(
+    channelId,
+    orderId,
+    txnAmount,
+    userInfo
+  );
+
+  var paymentDetail = paymentDetailBuilder.build();
+
+  Paytm.Payment.createTxnToken(paymentDetail).then((response) => {
+    if (response instanceof Paytm.SDKResponse) {
+      console.log("\nRaw Response:\n", response.getJsonResponse());
+    }
+
+    console.log(
+      "\nRESPONSE RECEIVED IN DEMOAPP: ",
+      response.getResponseObject()
     );
-    var userInfo = new Paytm.UserInfo(email);
-    userInfo.setAddress(address);
-    userInfo.setEmail(email);
-    userInfo.setFirstName(name);
-    userInfo.setMobile(mobile);
-    userInfo.setPincode(pin);
-
-    var goodsInfo = new Paytm.GoodsInfo();
-    goodsInfo.setMerchantGoodsId("goods_id1");
-    goodsInfo.setMerchantShippingId("shipping_id1");
-    goodsInfo.setSnapshotUrl("snapshot_url");
-    goodsInfo.setDescription("desc");
-    goodsInfo.setCategory("category");
-    goodsInfo.setQuantity("qty");
-    goodsInfo.setUnit("unit");
-    goodsInfo.setPrice(
-      Paytm.Money.constructWithCurrencyAndValue(Paytm.EnumCurrency.INR, "1.00")
-    );
-
-    var paymentDetailBuilder = new Paytm.PaymentDetailBuilder(
-      channelId,
-      orderId,
-      txnAmount,
-      userInfo
-    );
-
-    var paymentDetail = paymentDetailBuilder.build();
-
-    Paytm.Payment.createTxnToken(paymentDetail).then((response) => {
-      if (response instanceof Paytm.SDKResponse) {
-        console.log("\nRaw Response:\n", response.getJsonResponse());
-      }
-
-      console.log(
-        "\nRESPONSE RECEIVED IN DEMOAPP: ",
-        response.getResponseObject()
-      );
-      const txnToken = response.getResponseObject().body.txnToken;
-      res.send({ txnToken: txnToken, orderId: orderId });
-    });
-  } catch (e) {
-    console.log("Exception caught: ", e);
-    Paytm.LoggingUtil.addLog(
-      Paytm.LoggingUtil.LogLevel.INFO,
-      "App",
-      "Exception caught: ",
-      e
-    );
-    return Promise.reject(e);
-  }
+    const txnToken = response.getResponseObject().body.txnToken;
+    res.send({ txnToken: txnToken, orderId: orderId });
+  });
 }
