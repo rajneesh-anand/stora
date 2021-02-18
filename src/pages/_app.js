97730977695acb04ef4e08acf314db064bf02d6a
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-02-18 22:17:30 +0530

    paytm

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ const MyApp = ({ Component, pageProps, reduxStore, session }) => {
     <Provider store={reduxStore}>
       <AuthProvider
         session={session}
-        options={{ site: "http://localhost:3000" }}
+        options={{ site: "https://stora-eight.vercel.app" }}
       >
         <PersistGate loading={<Loading />} persistor={persistor}>
           <Component {...pageProps} />
